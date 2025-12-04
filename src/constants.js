7d13b9d2df138ab0a20c74ed5a75a817139a862d
sagar sharma <sagar9012sharma@gmail.com>
2025-12-04 10:57:40 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,301 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "TecchnoHacks Solution Pvt. Ltd",
+      date: "July 2025 - September",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Java Developer",
+      company: "Saiket Systems",
+      date: "Octuber 2025 - December 2025",
+      desc: "Contributed to innovative projects as a Java Developer role, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, Java SpringBoot and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "SpringBoot",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "IIMT University, Meerut",
+      date: "Sept 2022 - July 2026",
+      grade: "7.81 CGPA",
+      desc: "I have completed my Bachelor of Technology (B.Tech) in Computer Science and Engineering from IIMT University, Meerut. During my time at IIMT, I built a strong foundation in programming, software development, and core computer science principles. I studied key subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, Computer Networks, and Software Engineering. I actively participated in various workshops and technical events, which helped enhance my practical skills and broaden my technical knowledge. My experience at IIMT University has played a significant role in shaping my technical abilities and supporting my professional growth..",
+      degree: "Bachelor of Technology - B Tech(CSE)",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "GS Inter College, Doghat Baghpat",
+      date: "July 2020 - June 2022",
+      grade: "67%",
+      desc: "I completed my 12th grade (PCM – Physics, Chemistry, Mathematics) from GS Inter College, Doghat, Baghpat. During my studies, I built a strong foundation in science and mathematics, which helped me develop analytical thinking and problem-solving skills. Subjects like Physics, Chemistry, and Mathematics strengthened my logical understanding and prepared me for higher studies in the field of technology. My time at GS Inter College allowed me to participate in academic activities and work on assignments that helped me apply theoretical concepts in a practical way, contributing significantly to my overall learning and growth..",
+      degree: "Intermediate - 12 (PCM)",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "OBS Hr Sec School, Barnawa Baghpat",
+      date: "Apr 2019 - June 2020",
+      grade: "73%",
+      desc: "I completed my class 10 education from OBS Hr Sec School, Barnawa Baghpat under the Up board, where I studied Hindi, English, Maths, Science, Social-Science and Art.",
+      degree: "CBSE(X) ",
+    },
+    // {
+    //   // id: 3,
+    //   // img: vpsLogo,
+    //   // school: "Vatsalya Public School Govardhan, Mathura",
+    //   // date: "Apr 2015 - March 2016",
+    //   // grade: "87.5%",
+    //   // desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   // degree: "CBSE(X), Science with Computer Application",
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "CloudMate our Weather App",
+      description:
+        "The React Weather App is a modern, real-time weather checking application built using React.js. It allows users to search for any city and instantly view accurate weather details using a public weather API. The app focuses on clean UI, fast performance, and responsive design.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+      webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Rock Paper Scissor Game",
+      description:
+        "The Rock–Paper–Scissor Game is an interactive browser-based game built using HTML, CSS, and JavaScript. It allows the user to play the classic hand game against the computer. The computer's moves are generated randomly, making each round unpredictable and fun. The game features a clean user interface, dynamic score updates, and smooth interactivit.",
+      image: csprepLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/SagarSharma006/Rock_Paper_Scissor",
+      // webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Tic-Tac-Toe",
+      description:
+        "The Tic Tac Toe Game is a simple yet interactive two-player browser game built using HTML, CSS, and JavaScript. The game allows two players to play alternately using X and O on a 3×3 grid. It detects winning combinations, highlights the winner, and can reset for a new round. The game focuses on clean UI, efficient logic, and smooth user interaction.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/SagarSharma006/Tic-Tac-Toe",
+      // webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+  //   {
+  //     id: 3,
+  //     title: "Email Validator NPM Package",
+  //     description:
+  //       "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+  //     image: npmLogo,
+  //     tags: ["React JS", "Node.js", "NPM", "Validation"],
+  //     github: "https://github.com/codingmastr/cmtk-email-validator",
+  //     webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Task Reminder Chrome Extension Tool",
+  //     description:
+  //       "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+  //     image: taskremLogo,
+  //     tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+  //     github: "https://github.com/codingmastr/Task-Reminder-Tool",
+  //     webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Webverse Digital",
+  //     description:
+  //       "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+  //     image: webverLogo,
+  //     tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+  //     github: "https://github.com/codingmastr/Webverse-Digital",
+  //     webapp: "https://webversedigital.com/",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Coding Master",
+  //     description:
+  //       "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+  //     image: cmLogo,
+  //     tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+  //     github: "https://codingmasterweb.in/",
+  //     webapp: "https://codingmasterweb.in/",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "Image Search App",
+  //     description:
+  //       "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+  //     image: imagesearchLogo,
+  //     tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+  //     github: "https://github.com/codingmastr/Image-Search-App",
+  //     webapp: "https://imagsearch.netlify.app/",
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "Image Background Remover",
+  //     description:
+  //       "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+  //     image: removebgLogo,
+  //     tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+  //     github: "https://github.com/codingmastr/Image-Background-Remover",
+  //     webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
